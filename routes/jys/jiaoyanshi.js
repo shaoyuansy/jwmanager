4af1df5b39b43292226b62a77b793b34947817af
gituname <645541506@qq.com>
2016-09-20 15:27:31 +0800

    教研室

--- routes/jys/jiaoyanshi.js
+++ routes/jys/jiaoyanshi.js
@@ -6,11 +6,15 @@ var router = express.Router();
 var jysDao = require('../../models/dao/jw_jys/jysDao');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: '教务信息管理系统——教研室管理' });
 });
 
 router.get('/jys.html', function(req, res, next) {
-    res.render('jys/jiaoyanshi', { title: 'Express' });
+    res.render('jys/jiaoyanshi', { title: '教务信息管理系统——教研室管理' });
+});
+
+router.get('/_editJys.html', function(req, res, next) {
+    res.render('jys/_editJys', { title: '教务信息管理系统——教研室管理' });
 });
 
 
