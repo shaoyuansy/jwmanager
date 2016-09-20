a28cb3487db2f8e3d76460198f1aca25cc6f6823
gituname <645541506@qq.com>
2016-09-20 08:35:40 +0800

    Initial commit

---
+++ routes/master.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('master', { title: '教师信息管理系统' });
+});
+
+module.exports = router;
